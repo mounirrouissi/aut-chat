1eeef5b3d5103cdc5492daee079494a7134cfd8c
Monir11 <mounirrouissi2@gmail.com>
2025-09-22 19:21:52 +0200

    a

--- dsl-diagrams-back/src/main/java/com/dsl/service/MockCallInit.java
+++ dsl-diagrams-back/src/main/java/com/dsl/service/MockCallInit.java
@@ -4,20 +4,46 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.type.PhoneNumber;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
 
 public class MockCallInit {
     public static void main(String[] args) {
-        String url = "https://c24221ad324e.ngrok-free.app/api/twilio/voice";
-        Twilio.init("AC7e96d8d08434a57adcb77a58158d1478", "f7c0c6e605260ad9635587c716838997");
+        Properties props = new Properties();
 
+        try (InputStream input = MockCallInit.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            props.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        // Load properties
+        String accountSid = props.getProperty("twilio.accountSid");
+        String authToken = props.getProperty("twilio.authToken");
+        String fromNumber = props.getProperty("twilio.fromNumber");
+        String toNumber = props.getProperty("twilio.toNumber");
+        String url = props.getProperty("twilio.url");
+
+        // Init Twilio
+        Twilio.init(accountSid, authToken);
+
+        // Make call
         Call call = Call.creator(
-                new PhoneNumber("+21650695820"),
-                new PhoneNumber("+12135664564"),
+                new PhoneNumber(toNumber),   // recipient
+                new PhoneNumber(fromNumber), // sender (Twilio number)
                 URI.create(url)
         ).create();
 
-        call.getSid();
-
+        System.out.println("Call initiated, SID: " + call.getSid());
     }
 }
